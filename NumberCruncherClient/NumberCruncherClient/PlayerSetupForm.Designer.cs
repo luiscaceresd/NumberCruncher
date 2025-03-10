6d98ec042d2f7d36740f87c87942f2c21a7b2842
luiscaceresd <luiscaceresd97@gmail.com>
2025-03-10 00:38:01 -0300

    small change

--- NumberCruncherClient/NumberCruncherClient/PlayerSetupForm.Designer.cs
+++ NumberCruncherClient/NumberCruncherClient/PlayerSetupForm.Designer.cs
@@ -38,18 +38,19 @@
             // 
             // txtInitials
             // 
-            txtInitials.Location = new Point(73, 93);
+            txtInitials.Location = new Point(64, 70);
+            txtInitials.Margin = new Padding(3, 2, 3, 2);
             txtInitials.Name = "txtInitials";
-            txtInitials.Size = new Size(151, 27);
+            txtInitials.Size = new Size(133, 23);
             txtInitials.TabIndex = 7;
             // 
             // lblInitials
             // 
             lblInitials.AutoSize = true;
             lblInitials.Font = new Font("Stencil", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblInitials.Location = new Point(51, 47);
+            lblInitials.Location = new Point(45, 35);
             lblInitials.Name = "lblInitials";
-            lblInitials.Size = new Size(189, 18);
+            lblInitials.Size = new Size(155, 14);
             lblInitials.TabIndex = 6;
             lblInitials.Text = "Enter your initials:";
             // 
@@ -57,18 +58,19 @@
             // 
             lblDifficulty.AutoSize = true;
             lblDifficulty.Font = new Font("Stencil", 9F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
-            lblDifficulty.Location = new Point(396, 47);
+            lblDifficulty.Location = new Point(346, 35);
             lblDifficulty.Name = "lblDifficulty";
-            lblDifficulty.Size = new Size(164, 18);
+            lblDifficulty.Size = new Size(135, 14);
             lblDifficulty.TabIndex = 8;
             lblDifficulty.Text = "Select Difficulty";
             // 
             // btnEasy
             // 
             btnEasy.Font = new Font("Stencil", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnEasy.Location = new Point(401, 98);
+            btnEasy.Location = new Point(351, 74);
+            btnEasy.Margin = new Padding(3, 2, 3, 2);
             btnEasy.Name = "btnEasy";
-            btnEasy.Size = new Size(153, 70);
+            btnEasy.Size = new Size(134, 52);
             btnEasy.TabIndex = 9;
             btnEasy.Text = "Easy";
             btnEasy.UseVisualStyleBackColor = true;
@@ -77,9 +79,10 @@
             // btnModerate
             // 
             btnModerate.Font = new Font("Stencil", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnModerate.Location = new Point(401, 174);
+            btnModerate.Location = new Point(351, 130);
+            btnModerate.Margin = new Padding(3, 2, 3, 2);
             btnModerate.Name = "btnModerate";
-            btnModerate.Size = new Size(153, 70);
+            btnModerate.Size = new Size(134, 52);
             btnModerate.TabIndex = 10;
             btnModerate.Text = "Moderate";
             btnModerate.UseVisualStyleBackColor = true;
@@ -88,9 +91,10 @@
             // btnDifficult
             // 
             btnDifficult.Font = new Font("Stencil", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnDifficult.Location = new Point(401, 250);
+            btnDifficult.Location = new Point(351, 188);
+            btnDifficult.Margin = new Padding(3, 2, 3, 2);
             btnDifficult.Name = "btnDifficult";
-            btnDifficult.Size = new Size(153, 70);
+            btnDifficult.Size = new Size(134, 52);
             btnDifficult.TabIndex = 11;
             btnDifficult.Text = "Difficult";
             btnDifficult.UseVisualStyleBackColor = true;
@@ -98,16 +102,18 @@
             // 
             // PlayerSetupForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(623, 399);
+            ClientSize = new Size(545, 299);
             Controls.Add(btnDifficult);
             Controls.Add(btnModerate);
             Controls.Add(btnEasy);
             Controls.Add(lblDifficulty);
             Controls.Add(txtInitials);
             Controls.Add(lblInitials);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "PlayerSetupForm";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "PlayerSetupForm";
             Load += PlayerSetupForm_Load;
             ResumeLayout(false);
--- NumberCruncherClient/NumberCruncherClient/obj/Debug/net8.0-windows/NumberCruncherClient.AssemblyInfo.cs
+++ NumberCruncherClient/NumberCruncherClient/obj/Debug/net8.0-windows/NumberCruncherClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumberCruncherClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbe6a598b5b83bec9830c4c83de0a5b9917b5867")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a23402542aed1191fe8abc7441bad5b884110b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumberCruncherClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NumberCruncherClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
